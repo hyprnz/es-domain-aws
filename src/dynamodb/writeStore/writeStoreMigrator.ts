d73e943f536a278449a31b5d99e7d103267c1062
BlairJ <blair.jacobs@hypr.co.nz>
2022-09-15 16:52:52 +1200

    refactor to use common migrator

--- src/dynamodb/writeStore/writeStoreMigrator.ts
+++ src/dynamodb/writeStore/writeStoreMigrator.ts
@@ -1,8 +1,9 @@
 import { AWSError, DynamoDB } from 'aws-sdk'
+import { commonDynamoMigrator } from '../migrate/commonDynamoMigrator'
 
 export const makeWriteStoreMigrator = (client: DynamoDB, tableName: string = 'eventstore') => {
 
-  const params: DynamoDB.Types.CreateTableInput = {
+  const tableShape: DynamoDB.Types.CreateTableInput = {
     TableName: tableName,
     KeySchema: [
       {
@@ -30,41 +31,5 @@ export const makeWriteStoreMigrator = (client: DynamoDB, tableName: string = 'ev
     },
   }
 
-  async function up(): Promise<void> {
-    // logger.debug("### Migrating... ###");
-    try {
-
-
-      const exists = (await client.listTables().promise()).TableNames!.includes(tableName)
-      if (!exists) {
-        await client.createTable(params).promise()
-      }
-      // logger.debug("### Migration complete! ###");
-    } catch (e) {
-      // logger.error(new NestedError("There was an error creating the DB:", e));
-      console.error('Failed to create DrnamoDb Table', tableName, e)
-      throw e
-    }
-  }
-
-  async function down(): Promise<void> {
-    // logger.debug("### Deleting DB... ###");
-    try {
-      await (client.deleteTable({ TableName: tableName }).promise())
-    } catch (e) {
-
-      console.error('There was an error deleting the DB')
-      if(isAWSError(e)){
-        if(e.code === 'ResourceNotFoundException') return
-
-      }
-    }
-  }
-
-  return { up, down }
-}
-
-function isAWSError(e: unknown) : e is AWSError {
-  const maybeAWSError = e as Partial<AWSError>
-  return !!(maybeAWSError.code && maybeAWSError.message)
+  return commonDynamoMigrator(client, tableShape)
 }
